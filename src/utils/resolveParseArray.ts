24a47713223243ea65d0753d8e5036cc6c2c7dcd
Krivega Dmitriy <mr.krivega@gmail.com>
2023-06-26 14:02:08 +0300

    chore: update dependencies

--- src/utils/resolveParseArray.ts
+++ src/utils/resolveParseArray.ts
@@ -1,20 +1,16 @@
 // @ts-nocheck
-import flow from 'lodash/flow';
-import curryRight from 'lodash/curryRight';
-import map from 'lodash/map';
-import sortBy from 'lodash/sortBy';
-import property from 'lodash/property';
 import parseObject from './parseObject';
 
-const resolveParseArray = <T = any>(prop: string) => {
-  const parseArray = flow(
-    parseObject,
-    (arr) => {
-      return sortBy(arr, property(prop));
-    },
-    // @ts-ignore
-    curryRight(map)(parseObject)
-  ) as (array: T[]) => T[];
+const resolveParseArray = <T>(prop: string) => {
+  const parseArray = (array: T[]) => {
+    const parsedArray: T[] = array.map(parseObject);
+
+    parsedArray.sort((a, b) => {
+      return parseFloat(a[prop]) - parseFloat(b[prop]);
+    });
+
+    return parsedArray;
+  };
 
   return parseArray;
 };
